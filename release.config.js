292d546f876c68d5b2452c863eab15bd31552e3f
Jim Park <jimp@scribd.com>
2021-03-09 21:08:01 -0800

    chore: publish a release on dep updates

--- release.config.js
+++ release.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   "plugins": [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        "releaseRules": [
+          {"type": "deps", "release": "patch"}
+        ]
+      }
+    ],
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
