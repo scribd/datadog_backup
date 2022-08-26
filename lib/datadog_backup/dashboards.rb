571ff2853b1a352fafaf01c5e15924794807856f
Jim Park <jimp@scribd.com>
2022-08-26 10:22:48 -0700

    refactor: #all_dashboards only does one thing once

--- lib/datadog_backup/dashboards.rb
+++ lib/datadog_backup/dashboards.rb
@@ -3,10 +3,6 @@
 module DatadogBackup
   class Dashboards < Core
 
-    def all_dashboards
-      get_all.fetch('dashboards')
-    end
-
     def api_version
       'v1'
     end
@@ -18,7 +14,8 @@ module DatadogBackup
     def backup
       LOGGER.info("Starting diffs on #{::DatadogBackup::ThreadPool::TPOOL.max_length} threads")
 
-      futures = all_dashboards.map do |board|
+      dashboards = get_all.fetch('dashboards')
+      futures = dashboards.map do |board|
         Concurrent::Promises.future_on(::DatadogBackup::ThreadPool::TPOOL, board) do |board|
           id = board['id']
           get_and_write_file(id)
