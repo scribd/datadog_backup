a003595d9a3e889fd7794f83e39fbfd38b7d5183
Jim Park <jimp@scribd.com>
2022-10-26 12:54:34 -0700

    fix: restore multithreading for Dashboards.all

--- lib/datadog_backup/dashboards.rb
+++ lib/datadog_backup/dashboards.rb
@@ -11,16 +11,18 @@ module DatadogBackup
     @dig_in_list_body = 'dashboards'
 
     def self.all
-      @all ||= get_all.map do |resource|
+      return @all if @all
+
+      futures = get_all.map do |resource|
         Concurrent::Promises.future_on(DatadogBackup::ThreadPool::TPOOL, resource) do |r|
           new_resource(id: r.fetch(@id_keyname))
         end
       end
-      LOGGER.info "Found #{@all.length} #{@api_resource_name}s in Datadog"
+      LOGGER.info "Found #{futures.length} #{@api_resource_name}s in Datadog"
 
       watcher = DatadogBackup::ThreadPool.watcher
       watcher.join if watcher.status
-      Concurrent::Promises.zip(*@all).value!
+      @all = Concurrent::Promises.zip(*futures).value!
     end
   end
 end
