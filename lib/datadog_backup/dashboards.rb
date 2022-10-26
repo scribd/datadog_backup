9df4d97b5400c41541aaee15e9fd504fea4ba7e3
Jim Park <jimp@scribd.com>
2022-10-26 12:37:53 -0700

    fix: dashboards needs to do a get for each element in the list

--- lib/datadog_backup/dashboards.rb
+++ lib/datadog_backup/dashboards.rb
@@ -9,5 +9,13 @@ module DatadogBackup
     @banlist = %w[modified_at url].freeze
     @api_service = DatadogBackup::Client.new
     @dig_in_list_body = 'dashboards'
+
+    def self.all
+      @all ||= get_all.map do |resource|
+        new_resource(id: resource.fetch(@id_keyname))
+      end
+      LOGGER.info "Found #{@all.length} #{@api_resource_name}s in Datadog"
+      @all
+    end
   end
 end
