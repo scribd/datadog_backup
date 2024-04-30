0417d31f2e27ddf1b5bccba45415fcbd119abd2a
Kamran Farhadi <kamranf@scribd.com>
2024-04-29 22:35:10 -0400

    fix: Deprecate Ruby 3.0 and Drop support for Ruby 2.7 BREAKING CHANGE: ruby 2.7 is no longer supported. Please upgrade to ruby 3.0 or higher.

--- lib/datadog_backup/deprecations.rb
+++ lib/datadog_backup/deprecations.rb
@@ -4,7 +4,7 @@ module DatadogBackup
   # Notify the user if they are using deprecated features.
   module Deprecations
     def self.check
-      LOGGER.warn "ruby-#{RUBY_VERSION} is deprecated. Ruby 2.7 or higher will be required to use this gem after datadog_backup@v3" if RUBY_VERSION < '2.7'
+      LOGGER.warn "ruby-#{RUBY_VERSION} is deprecated. Ruby 3.1 or higher will be required to use this gem after datadog_backup@v3" if RUBY_VERSION < '3.1'
     end
   end
 end
